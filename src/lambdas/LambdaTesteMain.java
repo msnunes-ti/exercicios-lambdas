0bfa1d9137c921f16f892dd89c299d081e115eff
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-08-18 10:44:40 -0300

    Commit

---
+++ src/lambdas/LambdaTesteMain.java
@@ -0,0 +1,32 @@
+package lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaTesteMain {
+
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Marcelo");
+        stringList.add("Giseli");
+        stringList.add("Gabriel");
+        stringList.add("Jéssica");
+        stringList.add("Naidion");
+        stringList.add("Fernanda");
+        stringList.add("Cecilia");
+        stringList.add("Roberto");
+        stringList.add("Romeu");
+        System.out.println(stringList + " Lista original tem " + stringList.size() + " itens.");
+
+        System.out.println(ListaString.listStringToUpperCase(stringList) + " Para UpperCase");
+
+        System.out.println(ListaString.listStringContainsA(stringList)
+                + " Tamanho da lista contendo a letra (a): "
+                + ListaString.listStringContainsA(stringList).size());
+
+
+
+    }
+
+}
--- src/lambdas/ListaString.java
+++ src/lambdas/ListaString.java
@@ -1,4 +1,17 @@
 package lambdas;
 
+import java.util.List;
+
 public class ListaString {
+
+    public static List<String> listStringToUpperCase(List<String> stringList) {
+        return stringList.stream().map(String::toUpperCase).toList();
+    }
+
+    public static List<String> listStringContainsA (List<String> stringList) {
+        return stringList.stream().filter(s -> s.contains("a")).toList();
+    }
+
+
+
 }
