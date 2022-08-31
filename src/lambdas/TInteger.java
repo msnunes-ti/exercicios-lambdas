b66960ad82f4f44080c9ae8ad6b65ccb40305dc9
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-08-31 14:22:43 -0300

    Mais um método

--- src/lambdas/TInteger.java
+++ src/lambdas/TInteger.java
@@ -1,4 +1,34 @@
 package lambdas;
 
+import java.util.List;
+
 public class TInteger {
+
+    private String nome;
+
+    private List<Integer> integerList;
+
+    public TInteger() {
+    }
+
+    public TInteger(String nome, List<Integer> integerList) {
+        this.nome = nome;
+        this.integerList = integerList;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Integer> getIntegerList() {
+        return integerList;
+    }
+
+    public void setIntegerList(List<Integer> integerList) {
+        this.integerList = integerList;
+    }
 }
