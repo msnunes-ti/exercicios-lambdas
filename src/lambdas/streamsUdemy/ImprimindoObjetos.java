1df83748f627014a4a1e167afe19dbe004eb787b
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-09-19 19:13:45 -0300

    Commit

--- src/lambdas/streamsUdemy/ImprimindoObjetos.java
+++ src/lambdas/streamsUdemy/ImprimindoObjetos.java
@@ -1,6 +1,7 @@
 package lambdas.streamsUdemy;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ImprimindoObjetos {
@@ -9,6 +10,18 @@ public class ImprimindoObjetos {
 
         List<String> aprovados = Arrays.asList("Gabriel", "Giseli", "Jéssica", "Teste", "Marcelo");
 
+        System.out.println("Usando o foreach: ");
+        for(String nome: aprovados) {
+            System.out.println(nome);
+        }
+
+        System.out.println("\nUsando Iterator:");
+        Iterator<String> iterator = aprovados.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toUpperCase().replace("TESTE", "TROCADO"));
+        }
+
+        
 
 
     }
