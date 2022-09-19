421efc0643c7dd51f0814d355a246c112712f884
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-09-19 19:07:39 -0300

    Commit

--- src/lambdas/streamsUdemy/ImprimindoObjetos.java
+++ src/lambdas/streamsUdemy/ImprimindoObjetos.java
@@ -1,7 +1,16 @@
 package lambdas.streamsUdemy;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ImprimindoObjetos {
 
+    public static void main(String[] args) {
 
+        List<String> aprovados = Arrays.asList("Gabriel", "Giseli", "Jéssica", "Teste", "Marcelo");
+
+
+
+    }
 
 }
