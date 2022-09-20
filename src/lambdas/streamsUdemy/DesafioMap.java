6fa919db1588570c02d9f95df126ad2c15da2985
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-09-20 20:26:07 -0300

    Atualizado

--- src/lambdas/streamsUdemy/DesafioMap.java
+++ src/lambdas/streamsUdemy/DesafioMap.java
@@ -1,4 +1,27 @@
 package lambdas.streamsUdemy;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import static java.util.stream.Collectors.toList;
+
 public class DesafioMap {
+
+    public static void main(String[] args) {
+
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        System.out.println("Lista inicial: " + integerList);
+
+        List<String> stringList = integerList.stream().map(Integer::toBinaryString).toList();
+        System.out.println(stringList);
+
+        UnaryOperator<String> reverter = s -> new StringBuilder(s).reverse().toString();
+
+        List<String> stringList1 = stringList.stream().map(reverter).toList();
+        System.out.println(stringList1);
+
+
+    }
 }
+
