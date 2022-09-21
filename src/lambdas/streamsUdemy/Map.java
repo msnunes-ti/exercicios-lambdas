b9171cf890a148e92e816cec99d65dfe057b1fbd
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-09-21 11:28:31 -0300

    Atualizado

--- src/lambdas/streamsUdemy/Map.java
+++ src/lambdas/streamsUdemy/Map.java
@@ -1,11 +1,26 @@
 package lambdas.streamsUdemy;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
 public class Map {
 
     public static void main(String[] args) {
 
+        Consumer<String> print = System.out::print;
 
+        List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda");
+        marcas.stream().map(String::toUpperCase).forEach(print);
 
+        UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+
+        System.out.println("\nUsando Composição:");
+        marcas.stream()
+                .map(String::toUpperCase)
+                .map(primeiraLetra)
+                .map(n -> n + "!!! ")
+                .forEach(print);
     }
-
 }
