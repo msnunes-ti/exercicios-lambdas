74b438748ca7e22890b880af57fffc099a716efa
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-10-18 19:58:00 -0300

    Commit

--- src/lambdas/streamsUdemy/Funcao.java
+++ src/lambdas/streamsUdemy/Funcao.java
@@ -7,10 +7,9 @@ public class Funcao {
     public static void main(String[] args) {
 
         Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Ímpar";
-        System.out.println(parOuImpar.apply(33));
-
         Function<String, String> oResultadoE = valor -> "O resultado é: " + valor;
         Function<String, String> empolgado = valor -> valor + "!!!";
+        Function<String, String> duvida = valor -> valor + "???";
 
         String resultadoFinal =
                 parOuImpar.
@@ -19,6 +18,12 @@ public class Funcao {
                         apply(32);
         System.out.println(resultadoFinal);
 
+        String resultadoFinal2 =
+                parOuImpar.
+                        andThen(oResultadoE).
+                        andThen(duvida).
+                        apply(33);
+        System.out.println(resultadoFinal2);
     }
 
 }
